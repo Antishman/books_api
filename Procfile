ec7af6b2eb0a97b41afc24992461da5832f2505f
Anteneh <164549000+Antishman@users.noreply.github.com>
2024-11-26 12:48:42 +0300

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn books_api.wsgipip install gunicorn
--- books_api/settings.py
+++ books_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--n%q@0r%b%7#khrfdofnqxuy9z(e6c6*1zi6*0p=pjmwi$zn07'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
